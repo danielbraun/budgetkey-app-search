fa21eec975092889293b7f6a450cc02536595409
Daniel Braun <danielbraun@me.com>
2017-07-13 14:17:31 +0000

    Fix formatting and lint errors.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,25 +1,25 @@
 import './rxjs-extensions';
 
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
-import {HttpModule} from "@angular/http";
-import {SearchService} from "./_service/search.service";
-import {SearchComponent} from "./search/search.component";
+import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { SearchService } from './_service/search.service';
+import { SearchComponent } from './search/search.component';
 
 import { BudgetKeyCommonModule } from 'budgetkey-ng2-components';
-import {SearchResultBudgetComponent} from "./search_result/search_result.component";
-import {SearchResultChangesComponent} from "./search_result/search_result.component";
-import {SearchResultExemptionComponent} from "./search_result/search_result.component";
-import {SearchResultProcurementComponent} from "./search_result/search_result.component";
-import {SearchResultSupportsComponent} from "./search_result/search_result.component";
-import {SearchResultEntitiesComponent} from "./search_result/search_result.component";
+import { SearchResultBudgetComponent } from './search_result/search_result.component';
+import { SearchResultChangesComponent } from './search_result/search_result.component';
+import { SearchResultExemptionComponent } from './search_result/search_result.component';
+import { SearchResultProcurementComponent } from './search_result/search_result.component';
+import { SearchResultSupportsComponent } from './search_result/search_result.component';
+import { SearchResultEntitiesComponent } from './search_result/search_result.component';
 
-import {Highlighter} from "./highlighter/search.highlighter";
+import { Highlighter } from './highlighter/search.highlighter';
 
 @NgModule({
-  imports:      [
+  imports: [
     BrowserModule,
     HttpModule,
     BudgetKeyCommonModule
@@ -38,6 +38,6 @@ import {Highlighter} from "./highlighter/search.highlighter";
   providers: [
     SearchService
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
